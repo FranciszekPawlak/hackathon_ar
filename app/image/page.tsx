1053cd4be5cbc258848771d77225c3159357cc7f
Franciszek Pawlak <franciszek.pawlak@erecruiter.pl>
2023-12-06 15:59:39 +0100

    nara

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -6,39 +6,36 @@ export default function Page() {
     return (
         <>
             <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script
                     src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js"></script>
                 <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
             </head>
-            {typeof window !== "undefined" &&
-                <div style={{height: "100vh", width: "100vw", margin: 0, overflow: "hidden"}}>
-                    <div style={{height: "100%", width: "100%"}}>
-                        <a-scene
-                            vr-mode-ui="enabled: false;"
-                            renderer="logarithmicDepthBuffer: true; precision: medium;"
-                            embedded
-                            arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+            {typeof window !== "undefined" && 
+                <a-scene
+                    vr-mode-ui="enabled: false;"
+                    renderer="logarithmicDepthBuffer: true; precision: best;"
+                    arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+                >
+                    <a-nft
+                        type="nft"
+                        url="./qr/qr"
+                        smooth="true"
+                        smoothCount="10"
+                        smoothTolerance=".01"
+                        smoothThreshold="5"
+                    >
+                        <a-entity
+                            gltf-model="./square/Box.gltf"
+                            scale="100 100 100"
+                            position="0 0 0"
                         >
-
-                            <a-nft
-                                type="nft"
-                                url="./qr/qr"
-                                smooth="true"
-                                smoothCount="10"
-                                smoothTolerance=".01"
-                                smoothThreshold="5"
-                            >
-                                <a-entity
-                                    gltf-model="./square/Box.gltf"
-                                    scale="100 100 100"
-                                    position="0 0 0"
-                                >
-                                </a-entity>
-                            </a-nft>
-                            <a-entity camera></a-entity>
-                        </a-scene>
-                    </div>
-                </div>}
+                        </a-entity>
+                    </a-nft>
+                    <a-camera></a-camera>
+                </a-scene>
+                        
+                }
         </>
 
     )
