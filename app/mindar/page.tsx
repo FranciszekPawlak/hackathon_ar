f502e14c7225350def13cdeb65a7c4d3a83a8b0c
Kacper Arendt <kacper.arendt@erecruiter.pl>
2023-12-07 16:01:16 +0100

    mindar

--- app/mindar/page.tsx
+++ app/mindar/page.tsx
@@ -1,23 +1,8 @@
 'use client'
 
 export default function Page() {
-    const model = './buggy/Buggy.gltf'
-
-
-    const Mindar = () => <>
-        <a-scene mindar-image="imageTargetSrc: ./targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
-            <a-assets>
-                <a-asset-item id="avatarModel" src={model}></a-asset-item>
-            </a-assets>
-
-            <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-
-            <a-entity mindar-image-target="targetIndex: 0">
-                <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
-            </a-entity>
-        </a-scene>
-
-    </>
+    const model = './arrow/arrow.gltf'
+    const model1 = './square/Box.gltf'
 
 
     return (
@@ -28,10 +13,29 @@ export default function Page() {
             </head>
 
             <div style={{ height: "100vh", width: "100vw", margin: 0, overflow: "hidden" }}>
-                <div style={{ height: "100%", width: "100%" }}>
-                    <Mindar />
-                </div>
-            </div>
+                <a-scene mindar-image="imageTargetSrc: ./targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false" >
+                    <a-assets>
+                        <a-asset-item id="avatarModel" src={model} />
+                        <a-asset-item id="avatarModel1" src={model1} />
+                    </a-assets>
+
+                    <a-camera position="0 0 0" look-controls="enabled: false" />
+
+                    <a-entity mindar-image-target="targetIndex: 0">
+                        <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.105 0.105 0.105" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" />
+                    </a-entity>
+                    <a-entity mindar-image-target="targetIndex: 1">
+                        <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.105 0.105 0.105" src="#avatarModel1" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" />
+                    </a-entity>
+
+                    {/* <a-entity mindar-image-target="targetIndex: 0">
+                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#avatarModel" animation-mixer>
+                    </a-entity>
+                    <a-entity mindar-image-target="targetIndex: 1">
+                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#avatarModel1" animation-mixer>
+                    </a-entity> */}
+                </a-scene>
+            </div >
         </>
 
     )
